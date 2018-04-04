45b1669a800a1723aaf3993ab1158e08b2d5a031
pxshl <zpxshl@gmai.com>
2018-04-04 09:04:57 +0800

    使用Glide,简化代码

---
+++ app/src/main/java/com/example/pxshl/weather/service/UpdateService.java
@@ -0,0 +1,8 @@
+package com.example.pxshl.weather.service;
+
+/**
+ * Created by pxshl on 18-4-3.
+ */
+
+public class UpdateService {
+}
---
+++ app/src/main/java/com/example/pxshl/weather/utils/UpdateTask.java
@@ -0,0 +1,8 @@
+package com.example.pxshl.weather.utils;
+
+/**
+ * Created by pxshl on 18-4-3.
+ */
+
+public class UpdateTask {
+}
